e8e2c21522033d7d05f299920117f46ac74ef231
Paul Nation <nonhermitian@gmail.com>
2020-06-17 19:45:14 -0400

    updated docs

--- kaleidoscope/_static/documentation_options.js
+++ kaleidoscope/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.4.dev4+56dd5d4',
+    VERSION: '0.0.4.dev4+e9fd9cb',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
