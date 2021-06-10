c78dca93fb5877eb6a950378283c9e8c94ae2765
Paul Nation <nonhermitian@gmail.com>
2021-06-10 05:39:02 -0400

    Update KAL docs

--- kaleido/_static/documentation_options.js
+++ kaleido/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.10',
+    VERSION: '0.0.13.dev13+3c1f2c3',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
